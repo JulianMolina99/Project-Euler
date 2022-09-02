de8ae85020e7b15eef8b541ecfafc26e49c851c1
Julian Steven Molina Campos <110347693+JulianMolina99@users.noreply.github.com>
2022-09-01 21:55:46 -0500

    Update 05_Smallest_multiple.js

--- algorithms/JavaScript/05_Smallest_multiple.js
+++ algorithms/JavaScript/05_Smallest_multiple.js
@@ -1,6 +1,9 @@
-/* 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+/* 
+Problem 5: Smallest multiple
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to number? */
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to n?
+*/
 
 function smallestMult(number){
     let array = Array.from({length:number-1}, (v,i)=>{
